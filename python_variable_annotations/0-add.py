c3b37a4bbc79f3a39ebaf9bd90eb5b945f3f4782
cmeynet <clemence.meynet14@gmail.com>
2025-07-28 18:25:05 +0200

    create 0-add.py 0-main.py README.md

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Function add that takes a float a and a float b as arguments
+and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Add two float and return a float """
+    return a + b

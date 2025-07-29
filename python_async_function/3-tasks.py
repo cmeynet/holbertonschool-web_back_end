319f20391369576c975e1891a04ae616946936b1
cmeynet <clemence.meynet14@gmail.com>
2025-07-29 15:57:42 +0200

    fix variable annotation for 3-tasks.py

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -9,7 +9,7 @@ import asyncio
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int) -> None:
+def task_wait_random(max_delay: int) -> Task[float]:
     """
     Create and return an asyncio.Task for wait_random.
 

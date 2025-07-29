776cdc16c61f6ff893d5387f1a30f58634343a8f
cmeynet <clemence.meynet14@gmail.com>
2025-07-29 12:43:07 +0200

    add variable annotation to 1-concurrent_coroutines

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,13 +3,14 @@
 Async routine called wait_n that takes in 2 int arguments
 (in this order): n and max_delay.
 """
+from typing import List
 import random
 import asyncio
 
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawn wait_random n times with the specified max_delay
     and return the list of delays in ascending order of completion.

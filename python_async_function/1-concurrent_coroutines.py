3be4d2d807cebe30937713d02f6a2d0ea6066d07
cmeynet <clemence.meynet14@gmail.com>
2025-07-29 12:25:43 +0200

    create 1-concurrent_coroutines.py

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Async routine called wait_n that takes in 2 int arguments
+(in this order): n and max_delay.
+"""
+import random
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def wait_n(n, max_delay):
+    """
+    Spawn wait_random n times with the specified max_delay
+    and return the list of delays in ascending order of completion.
+    """
+    tasks = []
+
+    for i in range(n):
+        task = asyncio.create_task(wait_random(max_delay))
+        tasks.append(task)
+
+    list_delays = []
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        list_delays.append(delay)
+
+    return list_delays

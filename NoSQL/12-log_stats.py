b9b7884be12da01b5149e60114c8a47caa1059ff
cmeynet <clemence.meynet14@gmail.com>
2025-08-13 12:48:00 +0200

    create 12-log_stats.py

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Stats about Nginx logs stored in MongoDB
+Database: logs
+Collection: nginx
+
+Display:
+- "logs: X"  where X is the number of documents in this collection
+- "Methods:"
+-   methods in this order:
+        method GET: <count>
+        method POST: <count>
+        method PUT: <count>
+        method PATCH: <count>
+        method DELETE: <count>
+- "status check: <count>" for documents with method=GET and path=/status
+"""
+from pymongo import MongoClient
+
+# Create a MongoDB client
+client = MongoClient("mongodb://127.0.0.1:27017")
+# Select the 'logs' database then the 'nginx' collection
+collection = client.logs.nginx
+
+print(f"""{collection.count_documents({})} logs
+Methods:
+\tmethod GET: {collection.count_documents({"method": "GET"})}
+\tmethod POST: {collection.count_documents({"method": "POST"})}
+\tmethod PUT: {collection.count_documents({"method": "PUT"})}
+\tmethod PATCH: {collection.count_documents({"method": "PATCH"})}
+\tmethod DELETE: {collection.count_documents({"method": "DELETE"})}
+{collection.count_documents
+ ({"method": "GET", "path": "/status"})} status check""")

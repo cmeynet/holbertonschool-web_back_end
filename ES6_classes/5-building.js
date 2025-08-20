5e62df9f09aa59e48f11c1d5f0dd321b07a33b31
cmeynet <clemence.meynet14@gmail.com>
2025-08-20 14:35:17 +0200

    fix 5-building.js method

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -5,6 +5,12 @@ export default class Building {
     }
 
     this._sqft = sqft;
+
+    // Si on est dans une sous-classe ET que la méthode n'a pas été surchargée → erreur
+    if (this.constructor !== Building
+      && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {

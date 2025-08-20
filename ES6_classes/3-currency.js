9b64349f7b96b1c6788a6eb6e20f3df6d8ebe1ca
cmeynet <clemence.meynet14@gmail.com>
2025-08-20 12:07:57 +0200

    create 3-currency.js

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,29 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter and Setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // Getter and Setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Method
+  displayFullCurrency() {
+    return `${this.name} ${(this.code)}`;
+  }
+}

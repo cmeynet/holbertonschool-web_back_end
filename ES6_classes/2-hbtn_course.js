3260cec9bdbe1de768b5601bd3f400463a97f92d
cmeynet <clemence.meynet14@gmail.com>
2025-08-20 11:47:00 +0200

    fix 2-hbtn_course.js group getter and setter

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,52 +2,53 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     // Type checks
     if (typeof name !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-      throw new TypeError('nength must be a number');
+      throw new TypeError('Length must be a number');
     }
     if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
-      throw new TypeError('students must be an array of strings');
+      throw new TypeError('Students must be an array of strings');
     }
 
     this._name = name;
     this._length = length;
-    this._students = [...students];
+    this._students = students;
   }
 
-  // Getters
+  // Getter and Setter for name
   get name() {
     return this._name;
   }
 
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this._students;
-  }
-
-  // Setters
   set name(newName) {
     if (typeof newName !== 'string') {
-      throw new TypeError('name must be a string');
+      throw new TypeError('Name must be a string');
     }
     this._name = newName;
   }
 
+  // Getter and Setter for length
+  get length() {
+    return this._length;
+  }
+
   set length(newLength) {
     if (typeof newLength !== 'number') {
-      throw new TypeError('length must be a number');
+      throw new TypeError('Length must be a number');
     }
     this._length = newLength;
   }
 
+  // Getter et Setter for students
+  get students() {
+    return this._students;
+  }
+
   set students(newStudents) {
     if (!Array.isArray(newStudents) || !newStudents.every((s) => typeof s === 'string')) {
-      throw new TypeError('students must be an array of strings');
+      throw new TypeError('Students must be an array of strings');
     }
-    this._students = [...newStudents];
+    this._students = newStudents;
   }
 }

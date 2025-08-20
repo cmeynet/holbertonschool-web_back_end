80ce2f0c4172a7fb2dc4f427908a0ff3c730e74c
cmeynet <clemence.meynet14@gmail.com>
2025-08-20 15:28:03 +0200

    fix 5-building.js constructor

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,11 +2,6 @@ export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
 
-    if (typeof sqft !== 'number') {
-      throw new TypeError('sqft must be a number');
-    }
-
-    // Si on est dans une sous-classe ET que la méthode n'a pas été surchargée → erreur
     if (this.constructor !== Building
       && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');

1cae3b8c6f6be4cc532fd2ebd2522c1e6543951e
cmeynet <clemence.meynet14@gmail.com>
2025-08-26 15:34:35 +0200

    fix 2-then.js add ()

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then({ status: 200, body: 'success' });
-  promise.catch(new Error(''));
-  promise.finally(console.log('Got a response from the API'));
-
-  return promise;
+  return promise
+    .then(() => (
+      { status: 200, body: 'success' }
+    ))
+    .catch(() => (
+      new Error('')
+    ))
+    .finally(() => (
+      console.log('Got a response from the API')
+    ));
 }

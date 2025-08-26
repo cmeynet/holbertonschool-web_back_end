3f7bb9d1d5447dae86b550470e8ff470bc516898
cmeynet <clemence.meynet14@gmail.com>
2025-08-26 11:10:09 +0200

    fix 8-clean_set.js add condition

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,12 @@
 export default function updateUniqueItems(map) {
-  if (!map instanceof Map) {
+  if (!(map instanceof Map)) {
     throw new Error('Cannot process');
   }
 
-  for (let [key, value] of map) {
+  for (const [key, value] of map) {
     if (value === 1) {
-        value = 100;
+      map.set(key, 100);
     }
-    map.set(key, value);
-}
-
-    return {...map};
+  }
+  return { ...map };
 }

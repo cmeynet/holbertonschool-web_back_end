b3214c4c77666b375dea02e7ed2153629e1594f7
cmeynet <clemence.meynet14@gmail.com>
2025-09-04 10:51:36 +0200

    fix 7-http_express.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,24 +1,59 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n');
+      const cleanLines = lines.filter((line) => line.trim() !== '');
+      const rows = cleanLines.slice(1);
+      const students = rows.map((line) => line.split(','));
+
+      const totalStudents = students.length;
+      let output = `Number of students: ${totalStudents}\n`;
+
+      const fields = {};
+      for (const student of students) {
+        const firstname = student[0];
+        const field = student[3];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+
+      for (const fieldName of Object.keys(fields)) {
+        const names = fields[fieldName].join(', ');
+        output += `Number of students in ${fieldName}: ${fields[fieldName].length}. List: ${names}\n`;
+      }
+      resolve(output.trim());
+    });
+  });
+}
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-    const databasePath = process.argv[2];
-
-    countStudents(databasePath)
-      .then((texte) => {
-        res.send(`This is the list of our students\n${texte}`);
-      })
-      .catch(() => {
-        res.send('Cannot load the database');
-      });
+app.get('/students', async (req, res) => {
+  try {
+    const dataStudents = await countStudents(process.argv[2]);
+    res.setHeader('Content-type', 'text/plain');
+    res.status(200).send(`This is the list of our students\n${dataStudents}`);
+  } catch (err) {
+    res.send(err.message);
+  }
 });
 
-app.listen(1245);
+if (require.main === module) {
+  app.listen(1245);
+}
 
 module.exports = app;

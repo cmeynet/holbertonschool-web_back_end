7d07affc649965777b42fe9ca5d237ba7e9232f8
cmeynet <clemence.meynet14@gmail.com>
2025-09-04 10:11:13 +0200

    fix 6-http_express.js add if require = main

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
-})
+});
 
-app.listen(1245);
+if (require.main === module) {
+  app.listen(1245);
+}
 
 module.exports = app;
